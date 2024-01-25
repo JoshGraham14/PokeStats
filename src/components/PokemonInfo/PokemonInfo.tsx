41e8a82d7805ccc648c74e05343546f866625d04
Josh Graham <joshgraham14@gmail.com>
2024-01-25 14:50:12 -0500

    Replaced sprite-related content with SpriteContainer component

--- src/components/PokemonInfo/PokemonInfo.tsx
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query'
 import { fetchPokemon } from '../../api'
 import styles from './PokemonInfo.module.css'
 import { capitalize } from '../../util'
+import { SpriteContainer } from '../SpriteContainer/SpriteContainer'
 
 export const PokemonInfo = (props: { url: string }) => {
 	const { url } = props
@@ -14,26 +15,7 @@ export const PokemonInfo = (props: { url: string }) => {
 	return (
 		<div>
 			<h2>{capitalize(data.name)}</h2>
-			<span>
-				<img
-					src={data.sprites.front_default}
-					alt={`${data.name}'s front sprite`}
-				/>
-				<img
-					src={data.sprites.back_default}
-					alt={`${data.name}'s back sprite`}
-				/>
-			</span>
-			<span>
-				<img
-					src={data.sprites.front_shiny}
-					alt={`${data.name}'s shiny front sprite`}
-				/>
-				<img
-					src={data.sprites.back_shiny}
-					alt={`${data.name}'s shiny back sprite`}
-				/>
-			</span>
+			<SpriteContainer name={data.name} sprites={data.sprites} />
 		</div>
 	)
 }
