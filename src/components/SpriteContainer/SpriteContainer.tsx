7c72f219d15d745b9f78346acb6174d390b924d1
Josh Graham <joshgraham14@gmail.com>
2024-01-25 14:56:22 -0500

    Initial basic Sprite Container

---
+++ src/components/SpriteContainer/SpriteContainer.tsx
@@ -0,0 +1,35 @@
+import type { Sprites } from '../../types'
+
+import styles from './SpriteContainer.module.css'
+
+interface Props {
+	name: string
+	sprites: Sprites
+}
+
+export const SpriteContainer = (props: Props) => {
+	const { name, sprites } = props
+
+	return (
+		<div className={styles.spriteContainer}>
+			<h3>Sprites</h3>
+			<span>
+				<img
+					src={sprites.front_default}
+					alt={`${name}'s front sprite`}
+				/>
+				<img src={sprites.back_default} alt={`${name}'s back sprite`} />
+			</span>
+			<span>
+				<img
+					src={sprites.front_shiny}
+					alt={`${name}'s shiny front sprite`}
+				/>
+				<img
+					src={sprites.back_shiny}
+					alt={`${name}'s shiny back sprite`}
+				/>
+			</span>
+		</div>
+	)
+}
