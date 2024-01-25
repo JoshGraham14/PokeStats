c73da53fd4641d4834028a5c14dffc0c1bc7d739
Josh Graham <joshgraham14@gmail.com>
2024-01-25 17:48:15 -0500

    Added BaseStats component

--- src/components/PokemonInfo/PokemonInfo.tsx
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -3,6 +3,7 @@ import { fetchPokemon } from '../../api'
 import styles from './PokemonInfo.module.css'
 import { capitalize } from '../../util'
 import { SpriteContainer } from '../SpriteContainer/SpriteContainer'
+import { BaseStats } from '../BaseStats/BaseStats'
 
 export const PokemonInfo = (props: { url: string }) => {
 	const { url } = props
@@ -13,9 +14,10 @@ export const PokemonInfo = (props: { url: string }) => {
 	if (error) return <div>Something went wrong</div>
 
 	return (
-		<div>
+		<div className={styles.infoWrapper}>
 			<h2>{capitalize(data.name)}</h2>
 			<SpriteContainer name={data.name} sprites={data.sprites} />
+			<BaseStats stats={data.stats} />
 		</div>
 	)
 }
