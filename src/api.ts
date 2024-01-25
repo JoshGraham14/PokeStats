88f3c1f2c554447c6e7265ad6719ce78fc2a2826
Josh Graham <joshgraham14@gmail.com>
2024-01-25 00:02:19 -0500

    Created fetchPokemon(url: string) function

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import type { APIResponse, PokemonPreview } from './types'
+import type { APIResponse, PokemonPreview, Pokemon } from './types'
 
 export const fetchAllPokemon = async (): Promise<PokemonPreview[]> => {
 	const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
@@ -7,4 +7,14 @@ export const fetchAllPokemon = async (): Promise<PokemonPreview[]> => {
 	}
 	const data: APIResponse<PokemonPreview> = await response.json()
 	return data.results
+}
+
+export const fetchPokemon = async (url: string): Promise<Pokemon> => {
+	const response = await fetch(url)
+	if (!response.ok) {
+		throw new Error(response.statusText)
+	}
+
+	const data: Pokemon = await response.json()
+	return data
 }
