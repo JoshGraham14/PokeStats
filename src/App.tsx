8a78ba5bd00d54e84c7e4d5e35d0064997fdbc79
Josh Graham <joshgraham14@gmail.com>
2024-01-24 23:43:57 -0500

    Get pokemonUrl from SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { SearchBar } from './components/SearchBar/SearchBar'
 import './App.css'
+import { PokemonInfo } from './components/PokemonInfo/PokemonInfo'
 
 function App() {
+	const [pokemonUrl, setPokemonUrl] = useState<string>('')
 	const queryClient = new QueryClient()
 
 	return (
 		<QueryClientProvider client={queryClient}>
 			<h1>PokeStats</h1>
-			<SearchBar />
+			<SearchBar setPokemonUrl={setPokemonUrl} />
+			{pokemonUrl !== '' ? <PokemonInfo url={pokemonUrl} /> : null}
 		</QueryClientProvider>
 	)
 }
