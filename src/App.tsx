9055beea8edbc391eb561f84f19723e89168b8e0
Josh Graham <joshgraham14@gmail.com>
2024-01-25 23:12:18 -0500

    Added basic SkeletonTheme

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { SearchBar } from './components/SearchBar/SearchBar'
 import './App.css'
 import { PokemonInfo } from './components/PokemonInfo/PokemonInfo'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 function App() {
 	const [pokemonUrl, setPokemonUrl] = useState<string>('')
 	const queryClient = new QueryClient()
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			<h1>PokéStats</h1>
-			<SearchBar setPokemonUrl={setPokemonUrl} />
-			{pokemonUrl !== '' ? <PokemonInfo url={pokemonUrl} /> : null}
-		</QueryClientProvider>
+		<SkeletonTheme baseColor='#202020' highlightColor='#444'>
+			<QueryClientProvider client={queryClient}>
+				<h1>PokéStats</h1>
+				<SearchBar setPokemonUrl={setPokemonUrl} />
+				{pokemonUrl !== '' ? <PokemonInfo url={pokemonUrl} /> : null}
+			</QueryClientProvider>
+		</SkeletonTheme>
 	)
 }
 
