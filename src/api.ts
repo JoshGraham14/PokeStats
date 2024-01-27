271b52fa4a459e6448ad804006da34b2d651d46e
Josh Graham <joshgraham14@gmail.com>
2024-01-26 22:32:29 -0500

    Added fetchAbility function

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,9 @@
-import type { APIResponse, PokemonPreview, Pokemon } from './types'
+import type {
+	AbilityDetails,
+	APIResponse,
+	PokemonPreview,
+	Pokemon,
+} from './types'
 
 export const fetchAllPokemon = async (): Promise<PokemonPreview[]> => {
 	const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
@@ -16,5 +21,16 @@ export const fetchPokemon = async (url: string): Promise<Pokemon> => {
 	}
 
 	const data: Pokemon = await response.json()
+	return data
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const fetchAbility = async (url: string): Promise<AbilityDetails> => {
+	const response = await fetch(url)
+	if (!response.ok) {
+		throw new Error(response.statusText)
+	}
+
+	const data: AbilityDetails = await response.json()
 	return data
 }
