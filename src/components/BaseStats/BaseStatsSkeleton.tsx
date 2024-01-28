cdb915d81a16e592f48b434ed3efa47381333b39
Josh Graham <joshgraham14@gmail.com>
2024-01-28 00:54:39 -0500

    Adjusted spacing between bars

--- src/components/BaseStats/BaseStatsSkeleton.tsx
+++ src/components/BaseStats/BaseStatsSkeleton.tsx
@@ -4,17 +4,11 @@ import 'react-loading-skeleton/dist/skeleton.css'
 export const BaseStatsSkeleton = () => {
 	return (
 		<div>
-			{/* <h3>
-				<Skeleton
-					height={40}
-					style={{ marginBottom: '0.75rem', marginTop: '1.2rem' }}
-				/>
-			</h3> */}
 			<p>
 				<Skeleton
 					count={6}
 					height={20}
-					style={{ marginBottom: '1rem' }}
+					style={{ marginBottom: '1.1rem' }}
 				/>
 			</p>
 		</div>
