92a036bc326b68559dd72834e2ae8bd8f9938144
Josh Graham <joshgraham14@gmail.com>
2024-01-30 16:56:37 -0500

    Created useWindowSize hook for obtaining window dimensions

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,9 @@
-import React, { useState, RefObject } from 'react'
+import React, { useState, RefObject, useLayoutEffect } from 'react'
 
-export const useKeyPress = function (
+export const useKeyPress = (
 	targetKey: string,
 	ref: RefObject<HTMLInputElement>
-) {
+) => {
 	const [keyPressed, setKeyPressed] = useState(false)
 
 	const downHandler = ({ key }: { key: string }) => {
@@ -29,4 +29,17 @@ export const useKeyPress = function (
 	})
 
 	return keyPressed
+}
+
+export const useWindowSize = (): number[] => {
+	const [size, setSize] = useState([0, 0])
+	useLayoutEffect(() => {
+		function updateSize() {
+			setSize([window.innerWidth, window.innerHeight])
+		}
+		window.addEventListener('resize', updateSize)
+		updateSize()
+		return () => window.removeEventListener('resize', updateSize)
+	}, [])
+	return size
 }
