e6df2251950781459720ef141adfeba61fef15c1
Josh Graham <joshgraham14@gmail.com>
2024-01-30 17:00:26 -0500

    Now using useWindowSize hook instead of window.matchMedia

--- src/components/SpriteContainer/SpriteContainerSkeleton.tsx
+++ src/components/SpriteContainer/SpriteContainerSkeleton.tsx
@@ -1,29 +1,46 @@
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import styles from './SpriteContainer.module.css'
+import { useWindowSize } from '../../hooks'
 
 export const SpriteContainerSkeleton = () => {
-	const windowSize = window.matchMedia('(max-width: 500px)')
+	const [width] = useWindowSize()
 
 	// make sprites larger on mobile
-	const width = windowSize.matches ? 105 : 60
+	const spriteSize = width <= 500 ? 105 : 60
 
 	return (
 		<div className={styles.skeletonWrapper}>
 			<div className={styles.skeletonFlex}>
 				<div>
-					<Skeleton circle={true} width={width} height={width} />
+					<Skeleton
+						circle={true}
+						width={spriteSize}
+						height={spriteSize}
+					/>
 				</div>
 				<div>
-					<Skeleton circle={true} width={width} height={width} />
+					<Skeleton
+						circle={true}
+						width={spriteSize}
+						height={spriteSize}
+					/>
 				</div>
 			</div>
 			<div className={styles.skeletonFlex}>
 				<div>
-					<Skeleton circle={true} width={width} height={width} />
+					<Skeleton
+						circle={true}
+						width={spriteSize}
+						height={spriteSize}
+					/>
 				</div>
 				<div>
-					<Skeleton circle={true} width={width} height={width} />
+					<Skeleton
+						circle={true}
+						width={spriteSize}
+						height={spriteSize}
+					/>
 				</div>
 			</div>
 		</div>
